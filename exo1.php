ed645e24016bbb7313f0343cd0fd88656ca37ddb
paulinegallezot <paulinegallezot@gmail.com>
2021-06-13 20:30:14 +0200

    commit

--- exo1.php
+++ exo1.php
@@ -8,7 +8,7 @@ $months = array('janvier','fevrier','mars','avril','mai','juin','juillet','aout'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice 1</title>
+    <title>Exercice </title>
 </head>
 <body>
    <?php
